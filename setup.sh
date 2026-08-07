fc312ca0fd2a1d794a9e90fc32d3c2448664170f
dvost04 <dvost04@gmail.com>
2026-08-07 18:33:36 -0400

    Refactor setup.sh to improve script reliability

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-mkdir -p ~/.streamlit
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS = false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+set -e
+mkdir -p "$HOME/.streamlit"
+echo "[server]\nheadless = true\nenableCORS = false\nport = $PORT" > "$HOME/.streamlit/config.toml"
